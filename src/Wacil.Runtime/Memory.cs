e435a8469f40f919cd7e1a35c1bc0edfc6200e59
David N <davidcalieln@gmail.com>
2022-05-29 00:10:41 -0700

    Add aligned i32.load runtime implementation

--- src/Wacil.Runtime/Memory.cs
+++ src/Wacil.Runtime/Memory.cs
@@ -1,6 +1,7 @@
 namespace Wacil.Runtime {
     using System;
     using System.Collections.Generic;
+    using System.Runtime.CompilerServices;
 
     /// Represents a WebAssembly linear memory.
     public sealed class Memory {
@@ -24,11 +25,40 @@ namespace Wacil.Runtime {
         /// <summary>The maximum number of pages in this linear memory, or <c>-1</c> if there is no maximum number.</summary>
         public int MaximumPageCount { get; init; }
 
-        private readonly record struct Location(int page, ushort offset);
+        private readonly record struct Location(int Page, ushort Offset) {
+            /// The remaining number of bytes in the current page.
+            internal int RemainingBytes => PageSize - Offset;
 
-        private static Location GetPageIndex(uint address) {
-            // Math.DivRem
-            return new((int)(address / (uint)PageSize), (ushort)(address % (uint)PageSize));
+            internal static Location FromAddress(uint address) {
+                // Lower bits of address contain the actual offset from the start of the page
+                return new((int)(address >> 16), unchecked((ushort)(address)));
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private int ReadInt32Slow(Location location) {
+
+        }
+
+        //public static void Grow(Memory memory, uint pages)
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int ReadInt32(Memory memory, uint offset, uint alignment, uint address) {
+            var location = Location.FromAddress(offset + address);
+            // Is the alignment hint >= 4 bytes and is the location a multiple of 4?
+            if (alignment >= 2 && (location.Offset & 0b11) != 0) {
+                // An aligned read can occur
+                // TODO: Could eleminate fixed boilerplate by using unmanaged buffers.
+                //unsafe {
+                //    fixed(byte* page = memory.pages[location.Page]) {
+                //        return Unsafe.Read<int>((void*)(page + location.Offset));
+                //    }
+                //}
+                // Temporary slower implementation
+                return BitConverter.ToInt32(memory.pages[location.Page]);
+            } else {
+                return memory.ReadInt32Slow(location);
+            }
         }
     }
 }
