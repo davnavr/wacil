1305ce6405946b8fd1fc12a4e549239cbe9b6bff
David N <davidcalieln@gmail.com>
2022-06-02 16:55:21 -0700

    Update test.fsx to call function to test call_indirect

--- test/wat/function_pointers/test.fsx
+++ test/wat/function_pointers/test.fsx
@@ -1,13 +1,6 @@
-#r "nuget: Unquote"
 #r "./out/Wacil.Runtime.dll"
 #r "./out/function_pointers.dll"
 
-open Swensen.Unquote
-
 let instance = function_pointers()
-instance.functions[0] <- System.Action<int>(printfn "I got %i")
-
-// type Thing = delegate of int -> unit
-// let a = instance.functions[0]
-// let b = System.Delegate.CreateDelegate(typeof<Thing>, a.Target, a.Method) :?> Thing
-// b.Invoke 5
+instance.Functions[0] <- System.Action<int>(printfn "I got %i")
+instance.DoIt()
