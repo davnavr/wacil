64fefd5ca5e6fcaaf3462550b205bcbda82905aa
David N <davidcalieln@gmail.com>
2022-06-27 18:28:40 -0700

    Fix incorrect operands for starg and stloc

--- src/Wacil.Compiler/Emit/Module.fs
+++ src/Wacil.Compiler/Emit/Module.fs
@@ -27,8 +27,9 @@ type CilInstruction with
         | 1 -> CilInstruction CilOpCodes.Ldloc_1
         | 2 -> CilInstruction CilOpCodes.Ldloc_2
         | 3 -> CilInstruction CilOpCodes.Ldloc_3
-        | _ when index <= 255 -> CilInstruction(CilOpCodes.Ldloc_S, index)
-        | _ -> CilInstruction(CilOpCodes.Ldloc, index)
+        | _ when index < 0 -> raise(System.ArgumentOutOfRangeException(nameof index))
+        | _ when index <= 255 -> CilInstruction(CilOpCodes.Ldloc_S, uint8 index)
+        | _ -> CilInstruction(CilOpCodes.Ldloc, Checked.uint16 index)
 
     static member CreateLdarg index =
         match index with
@@ -36,8 +37,9 @@ type CilInstruction with
         | 1 -> CilInstruction CilOpCodes.Ldarg_1
         | 2 -> CilInstruction CilOpCodes.Ldarg_2
         | 3 -> CilInstruction CilOpCodes.Ldarg_3
-        | _ when index <= 255 -> CilInstruction(CilOpCodes.Ldarg_S, index)
-        | _ -> CilInstruction(CilOpCodes.Ldarg, index)
+        | _ when index < 0 -> raise(System.ArgumentOutOfRangeException(nameof index))
+        | _ when index <= 255 -> CilInstruction(CilOpCodes.Ldarg_S, uint8 index)
+        | _ -> CilInstruction(CilOpCodes.Ldarg, Checked.uint16 index)
 
     static member CreateStloc index =
         match index with
@@ -45,13 +47,15 @@ type CilInstruction with
         | 1 -> CilInstruction CilOpCodes.Stloc_1
         | 2 -> CilInstruction CilOpCodes.Stloc_2
         | 3 -> CilInstruction CilOpCodes.Stloc_3
-        | _ when index <= 255 -> CilInstruction(CilOpCodes.Stloc_S, index)
-        | _ -> CilInstruction(CilOpCodes.Stloc, index)
+        | _ when index < 0 -> raise(System.ArgumentOutOfRangeException(nameof index))
+        | _ when index <= 255 -> CilInstruction(CilOpCodes.Stloc_S, uint8 index)
+        | _ -> CilInstruction(CilOpCodes.Stloc, Checked.uint16 index)
 
     static member CreateStarg index =
         match index with
-        | _ when index <= 255 -> CilInstruction(CilOpCodes.Starg_S, index)
-        | _ -> CilInstruction(CilOpCodes.Starg, index)
+        | _ when index < 0 -> raise(System.ArgumentOutOfRangeException(nameof index))
+        | _ when index <= 255 -> CilInstruction(CilOpCodes.Starg_S, uint8 index)
+        | _ -> CilInstruction(CilOpCodes.Starg, Checked.uint16 index)
 
 [<NoComparison; NoEquality>]
 type RuntimeLibraryTable =
@@ -119,7 +123,7 @@ type BranchTargetStack =
     member this.PushLoop start =
         this.Targets.Add(BranchTarget.Loop(CilInstructionLabel start))
 
-    member this.PushIf() =
+    member this.PushIf(): CilInstructionLabel =
         let elseBranchTarget = CilInstructionLabel()
         this.Targets.Add(BranchTarget.If(elseBranchTarget, CilInstructionLabel()))
         elseBranchTarget
@@ -128,7 +132,7 @@ type BranchTargetStack =
         let target = this.Targets.Pop()
         target
 
-    member this.GetLabel (target: Wacil.Compiler.Wasm.Format.Index) =
+    member this.GetLabel (target: Wacil.Compiler.Wasm.Format.Index): CilInstructionLabel =
         match this.Targets.ItemFromEnd(Checked.int32 target) with
         | BranchTarget.Block label
         | BranchTarget.Loop label
