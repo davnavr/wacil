98c693a438d53dc5184ebb922f1b82cc105d941a
David N <davidcalieln@gmail.com>
2022-07-24 01:24:24 -0700

    Fix memory in function_pointers/tests.fsx

--- test/assemblyscript/function_pointers/tests.fsx
+++ test/assemblyscript/function_pointers/tests.fsx
@@ -9,6 +9,7 @@ open Swensen.Unquote
 do
     let instance = function_pointers.function_pointers(function_pointers.env(fun _ _ _ _ -> failwith "ABORT"))
     
+    Memory.Grow(1, instance.memory)
     Table.Grow(null, 1, instance.table)
     instance.table[0] <- System.Func<int64, int64>(fun a -> a + 5L) :> System.MulticastDelegate
     
