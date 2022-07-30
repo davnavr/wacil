7ad2663ea624c92ec0feda4666a8b5eb382052f4
David N <davidcalieln@gmail.com>
2022-07-30 12:53:29 -0700

    Update memory helpers to be generic methods

--- src/Wacil.Runtime/MemoryHelpers.cs
+++ src/Wacil.Runtime/MemoryHelpers.cs
@@ -3,52 +3,55 @@ namespace Wacil.Runtime;
 using System.Runtime.CompilerServices;
 
 /// <summary>Provides <see langword="static"/> methods for manipulating WebAssembly module memories.</summary>
-public static class MemoryHelpers<T> where T : IMemory32 {
+public static class MemoryHelpers {
     /// The size, in bytes, of a WebAssembly page.
     public const int PageSize = 65536;
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static int Grow(int delta, T memory) => memory.Grow(delta);
+    internal static int ToPageSize(int bytes) => bytes << 16;
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static byte ReadByte(int index, T memory, int offset) {
+    public static int Grow<T>(int delta, T memory) where T : IMemory32 => memory.Grow(delta);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static byte ReadByte<T>(int index, T memory, int offset) where T : IMemory32 {
         return memory[index + offset];
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static short ReadInt16(int index, T memory, int offset, byte alignmentPowerHint) {
+    public static short ReadInt16<T>(int index, T memory, int offset, byte alignmentPowerHint) where T : IMemory32 {
         return memory.ReadInt16(index + offset, alignmentPowerHint);
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static int ReadInt32(int index, T memory, int offset, byte alignmentPowerHint) {
+    public static int ReadInt32<T>(int index, T memory, int offset, byte alignmentPowerHint) where T : IMemory32 {
         return memory.ReadInt32(index + offset, alignmentPowerHint);
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static long ReadInt64(int index, T memory, int offset, byte alignmentPowerHint) {
+    public static long ReadInt64<T>(int index, T memory, int offset, byte alignmentPowerHint) where T : IMemory32 {
         return memory.ReadInt64(index + offset, alignmentPowerHint);
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static void Write(int index, byte value, T memory, int offset) {
+    public static void Write<T>(int index, byte value, T memory, int offset) where T : IMemory32 {
         memory[index + offset] = value;
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static void Write(int index, short value, T memory, int offset, byte alignmentPowerHint) {
+    public static void Write<T>(int index, short value, T memory, int offset, byte alignmentPowerHint) where T : IMemory32 {
         memory.Write(index + offset, alignmentPowerHint, value);
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static void Write(int index, int value, T memory, int offset, byte alignmentPowerHint) {
+    public static void Write<T>(int index, int value, T memory, int offset, byte alignmentPowerHint) where T : IMemory32 {
         memory.Write(index + offset, alignmentPowerHint, value);
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static void Write(int index, long value, T memory, int offset, byte alignmentPowerHint) {
+    public static void Write<T>(int index, long value, T memory, int offset, byte alignmentPowerHint) where T : IMemory32 {
         memory.Write(index + offset, alignmentPowerHint, value);
     }
 
-    public static void Write(T memory, int index, byte[] bytes) => memory.Write(index, bytes);
+    public static void Write<T>(T memory, int index, byte[] bytes) where T : IMemory32 => memory.Write(index, bytes);
 }
