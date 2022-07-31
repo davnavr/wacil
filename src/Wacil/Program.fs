391335633c403127f665f9c2e469b9a9b5812c40
David N <davidcalieln@gmail.com>
2022-07-31 14:29:30 -0700

    Add CLI option to write diagnostic errors to file

--- src/Wacil/Program.fs
+++ src/Wacil/Program.fs
@@ -9,26 +9,29 @@ open Wacil.Compiler.Emit
 
 type Options =
     | [<Unique; AltCommandLine("-f")>] Framework of TargetFramework
-    | Launch_Debugger
-    | [<Hidden>] Debug_Disassemble
     | [<Unique; AltCommandLine("-i")>] Module of ``module.wasm``: string
     | [<Unique>] Namespace of string
     | [<Unique; AltCommandLine("-o")>] Out of file: string
     //| [<Unique>] Type of OutputType
     //| [<Unique>] Version of Version
+    | [<Hidden>] Debug_Disassemble
+    | [<Unique>] Diagnostic_Output_Path of file: string
+    | Launch_Debugger
 
     interface IArgParserTemplate with
         member this.Usage =
             match this with
             | Framework _ -> "specifies the target framework of the assembly"
-            | Launch_Debugger -> "launches the debugger used to debug the compiler"
-            | Debug_Disassemble -> "disassembles the input WebAssembly file into the WebAssembly Text Format"
             | Module _ ->
                 "the WebAssembly file to convert into a CIL file, defaults to searching for a WebAssembly file in the current \
                 working directory if omitted"
             | Namespace _ ->
                 "the name of the namespace that will contain the class generated from the WebAssembly module"
             | Out _ -> "the path to the generated CIL file"
+            | Debug_Disassemble -> "disassembles the input WebAssembly file into the WebAssembly Text Format"
+            | Diagnostic_Output_Path _ ->
+                "specifies a path to a file where diagnostic information should be written to, defaults to standard error"
+            | Launch_Debugger -> "launches the debugger used to debug the compiler"
             //| Type _ -> "whether the generated CIL file is an assembly or module, defaults to generating an assembly"
 
 let parser = ArgumentParser.Create<Options>(programName = "wacil")
@@ -43,7 +46,7 @@ let main argv =
     try
         let args = parser.ParseCommandLine(inputs = argv, raiseOnUsage = true)
 
-        if args.Contains <@ Launch_Debugger @> then System.Diagnostics.Debugger.Launch() |> ignore
+        if args.Contains <@ Launch_Debugger @> then Diagnostics.Debugger.Launch() |> ignore
 
         let input =
             getFileArgument args <@ Module @> <| fun() -> 
@@ -78,7 +81,18 @@ let main argv =
         | Some framework -> options.TargetFramework <- framework
         | None -> ()
 
-        Diagnostics.handleInvalidCil (fun() -> Module.compileToStream options input'' writer) stderr
+        let disposeDiagnosticStream, diagnosticOutputStream =
+            match args.TryGetResult <@ Diagnostic_Output_Path @> with
+            | Some path ->
+                Directory.CreateDirectory(Path.GetDirectoryName path) |> ignore
+                true, new StreamWriter(path) :> TextWriter
+            | None ->
+                false, stderr
+
+        try
+            Diagnostics.handleInvalidCil (fun() -> Module.compileToStream options input'' writer) diagnosticOutputStream
+        finally
+            if disposeDiagnosticStream then diagnosticOutputStream.Close()
 
         0
     with
