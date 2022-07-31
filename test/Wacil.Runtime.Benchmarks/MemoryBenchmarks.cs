a4a76a1ef6b41c65eeee6c7cf2d46400a5eacf0c
David N <davidcalieln@gmail.com>
2022-07-30 17:14:05 -0700

    Update MemoryBenchmarks.cs

--- test/Wacil.Runtime.Benchmarks/MemoryBenchmarks.cs
+++ test/Wacil.Runtime.Benchmarks/MemoryBenchmarks.cs
@@ -3,21 +3,37 @@ namespace Wacil.Runtime.Benchmarks {
     using Wacil.Runtime;
 
     public class MemoryBenchmarks {
-        private readonly uint address;
+        private readonly int index;
         private readonly int data;
 
-        private readonly Memory memory = new(1);
+        private readonly ArrayMemory array = new ArrayMemory(new Limits(1));
+    
+        private readonly SegmentedMemory segmented = new SegmentedMemory(new Limits(1));
+    
+        private readonly UnmanagedMemory unmanaged = new UnmanagedMemory(new Limits(1));
 
         public MemoryBenchmarks() {
             var rng = new System.Random(42);
             data = rng.Next();
-            address = (uint)rng.Next(0, Memory.PageSize / 4) * 4;
+            index = (int)rng.Next(0, MemoryHelpers.PageSize / 4) * 4;
         }
 
         [Benchmark]
-        public int ReadInt32() => Memory.ReadInt32(address, memory, 0, 2);
+        public int ArrayReadInt32() => MemoryHelpers.ReadInt32<ArrayMemory>(index, array, 0, 2);
 
         [Benchmark]
-        public void WriteInt32() => Memory.WriteInt32(address, data, memory, 0, 2);
+        public void ArrayWriteInt32() => MemoryHelpers.Write<ArrayMemory>(index, data, array, 0, 2);
+
+        [Benchmark]
+        public int SegmentedReadInt32() => MemoryHelpers.ReadInt32<SegmentedMemory>(index, segmented, 0, 2);
+
+        [Benchmark]
+        public void SegmentedWriteInt32() => MemoryHelpers.Write<SegmentedMemory>(index, data, segmented, 0, 2);
+
+        [Benchmark]
+        public int UnmanagedReadInt32() => MemoryHelpers.ReadInt32<UnmanagedMemory>(index, unmanaged, 0, 2);
+
+        [Benchmark]
+        public void UnmanagedWriteInt32() => MemoryHelpers.Write<UnmanagedMemory>(index, data, unmanaged, 0, 2);
     }
 }
