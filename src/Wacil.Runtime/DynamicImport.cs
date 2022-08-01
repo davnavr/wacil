c6d7c6225a60c4bc60cb4133ee53b626b7e1409d
David N <davidcalieln@gmail.com>
2022-08-01 00:04:40 -0700

    Add class to dynamically construct imports

---
+++ src/Wacil.Runtime/DynamicImport.cs
@@ -0,0 +1,42 @@
+namespace Wacil.Runtime;
+
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+/// Constructs WASM module imports dynamically.
+public static class DynamicImport {
+    private static ConstructorInfo? GetImportConstructor<T>() {
+        foreach (var constructor in typeof(T).GetConstructors(BindingFlags.Public)) {
+            if (constructor.GetCustomAttribute<ImportConstructorAttribute>() != null) {
+                return constructor;
+            }
+        }
+
+        return null;
+    }
+
+    /// <summary>
+    /// Attempts to create a new instance of the <typeparamref name="T"/> class, providing the specified arguments.
+    /// </summary>
+    /// <exception cref="InvalidOperationException">
+    /// Thrown if the <typeparamref name="T"/> class does not contain a <see langword="public"/> constructor annotated with
+    /// the <see cref="ImportConstructorAttribute"/> attribute.
+    /// </exception>
+    public static T Create<T>(IReadOnlyDictionary<string, object> imports) where T : class {
+        ConstructorInfo? constructor = GetImportConstructor<T>();
+
+        if (constructor == null) {
+            throw new InvalidOperationException("No suitable constructor available for " + typeof(T).FullName);
+        }
+
+        ParameterInfo[] parameters = constructor.GetParameters();
+        var arguments = new object[parameters.Length];
+
+        for (int i = 0; i < arguments.Length; i++) {
+            arguments[i] = imports[parameters[i].Name ?? String.Empty];
+        }
+
+        return (T)constructor.Invoke(arguments);
+    }
+}
