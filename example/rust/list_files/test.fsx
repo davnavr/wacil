640d89a50f004b957b6debaf8b0035053dc2b9bf
David N <davidcalieln@gmail.com>
2022-08-04 12:42:42 -0700

    Add environment variable functions to Imports.cs

--- example/rust/list_files/test.fsx
+++ example/rust/list_files/test.fsx
@@ -3,16 +3,18 @@
 #r "./out/Wacil.Runtime.Wasi.dll"
 #r "./out/list_files.dll"
 
+open Wacil.Runtime
 open Wacil.Runtime.Wasi.Snapshot.Preview1
 
 do
     try
         let logger = Logger()
+        let env = EnvironmentVariables<ArrayMemory>()
         let instance =
             list_files.list_files(list_files.wasi_snapshot_preview1(
                 fd_write = logger.FdWrite Stubs.FdWrite,
-                environ_get = logger.EnvironGet Stubs.EnvironGet,
-                environ_sizes_get = logger.EnvironSizesGet Stubs.EnvironSizesGet,
+                environ_get = logger.EnvironGet(Imports.EnvironGet env),
+                environ_sizes_get = logger.EnvironSizesGet(Imports.EnvironSizesGet env),
                 proc_exit = ProcessExit.Throw
             ))
 
--- src/Wacil.Runtime.Wasi/Snapshot/Preview1/Imports.cs
+++ src/Wacil.Runtime.Wasi/Snapshot/Preview1/Imports.cs
@@ -11,6 +11,12 @@ public static class Imports {
     /// <summary>Provides the <c>args_sizes_get</c> function.</summary>
     public static Func<int, int, int> ArgsSizesGet<M>(CommandLineArguments<M> args) where M : IMemory32 => args.GetArgumentSizes;
 
+    /// <summary>Provides the <c>environ_get</c> function.</summary>
+    public static Func<int, int, int> EnvironGet<M>(EnvironmentVariables<M> env) where M : IMemory32 => env.GetVariables;
+
+    /// <summary>Provides the <c>environ_sizes_get</c> function.</summary>
+    public static Func<int, int, int> EnvironSizesGet<M>(EnvironmentVariables<M> env) where M : IMemory32 => env.GetVariableSizes;
+
     /// <summary>Provides the <c>fd_write</c> function.</summary>
     public static Func<int, int, int, int, int> FdWrite<M>(FileSystem<M> fs) where M : IMemory32 => fs.Write;
 
