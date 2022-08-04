51f263bba0a8677ab124ba5c65cb67e50c00db44
David N <davidcalieln@gmail.com>
2022-08-04 12:38:13 -0700

    Add environ_get implementation

--- src/Wacil.Runtime.Wasi/Snapshot/Preview1/EnvironmentVariables.cs
+++ src/Wacil.Runtime.Wasi/Snapshot/Preview1/EnvironmentVariables.cs
@@ -8,7 +8,7 @@ using Wacil.Runtime;
 
 /// <summary>Represents environment variables.</summary>
 /// <remarks>
-/// This class is meant to provide implementations for the <c>args_get</c> and <c>args_sizes_get</c> functions.
+/// This class is meant to provide implementations for the <c>environ_get</c> and <c>environ_sizes_get</c> functions.
 /// </remarks>
 public sealed class EnvironmentVariables<M> where M : IMemory32 {
     private const byte SEPARATOR = (byte)'=';
@@ -52,7 +52,9 @@ public sealed class EnvironmentVariables<M> where M : IMemory32 {
     private static void ValidateBytes(string name, ImmutableArray<byte> bytes) {
         foreach (byte value in bytes) {
             if (value == SEPARATOR) {
-                throw new ArgumentOutOfRangeException(name, "string should not contain a separator (the '=' character)");
+                throw new ArgumentOutOfRangeException(name, "String should not contain a separator (the '=' character)");
+            } else if (value == 0) {
+                throw new ArgumentOutOfRangeException(name, "String should not contain any null bytes");
             }
         }
     }
@@ -69,6 +71,56 @@ public sealed class EnvironmentVariables<M> where M : IMemory32 {
     public bool TryDefine(ImmutableArray<byte> name, ImmutableArray<byte> value) {
         ValidateBytes(nameof(name), name);
         ValidateBytes(nameof(value), value);
-        return variables.TryAdd(name, value);
+        bool added = variables.TryAdd(name, value);
+        if (added) {
+            totalByteSize += name.Length + value.Length + 2;
+        }
+        return added;
+    }
+
+    /// <summary>
+    /// Writes each environment variable into the specified <paramref name="buffer"/>, and the pointers to each individual variable into
+    /// the <paramref name="environ"/> array.
+    /// </summary>
+    /// <remarks>This provides an implementation for the <c>environ_get</c> function.</remarks>
+    public int GetVariables(int environ, int buffer) {
+        int environPointerIndex = environ;
+        int environBufferIndex = buffer;
+        foreach (var entry in variables) {
+            try {
+                memory.Write(environPointerIndex, 2, environBufferIndex); // Write the pointer to the key/value pair
+
+                var name = entry.Key.AsSpan();
+                memory.Write(environBufferIndex, name);
+                environBufferIndex += name.Length;
+
+                memory[environBufferIndex] = SEPARATOR;
+                environBufferIndex += 1;
+
+                var value = entry.Value.AsSpan();
+                memory.Write(environBufferIndex, value);
+                environBufferIndex += value.Length;
+
+                memory[environBufferIndex] = 0; // Write the null terminator
+                environBufferIndex += 1;
+            } catch (IndexOutOfRangeException) {
+                return (int)Errno.Fault;
+            }
+
+            environPointerIndex += 4;
+        }
+
+        return (int)Errno.Success;
+    }
+
+    /// <remarks>This provides an implementation for the <c>environ_sizes_get</c> function.</remarks>
+    public int GetVariableSizes(int variableCountPointer, int variableSizePointer) {
+        try {
+            memory.Write(variableCountPointer, 2, variables.Count);
+            memory.Write(variableSizePointer, 2, totalByteSize);
+            return (int)Errno.Success;
+        } catch (IndexOutOfRangeException) {
+            return (int)Errno.Fault;
+        }
     }
 }
