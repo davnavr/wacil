4297a5ba327c6ee48f282490eb6e07cd7a16599b
David N <davidcalieln@gmail.com>
2022-08-04 12:03:41 -0700

    Add try wrapper for rust/list_files/test.fsx

--- example/rust/list_files/test.fsx
+++ example/rust/list_files/test.fsx
@@ -6,12 +6,16 @@
 open Wacil.Runtime.Wasi.Snapshot.Preview1
 
 do
-    let logger = Logger()
-    let instance =
-        list_files.list_files(list_files.wasi_snapshot_preview1(
-            fd_write = logger.FdWrite Stubs.FdWrite,
-            environ_get = logger.EnvironGet Stubs.EnvironGet,
-            environ_sizes_get = logger.EnvironSizesGet Stubs.EnvironSizesGet,
-            proc_exit = ProcessExit.Throw
-        ))
-    ()
+    try
+        let logger = Logger()
+        let instance =
+            list_files.list_files(list_files.wasi_snapshot_preview1(
+                fd_write = logger.FdWrite Stubs.FdWrite,
+                environ_get = logger.EnvironGet Stubs.EnvironGet,
+                environ_sizes_get = logger.EnvironSizesGet Stubs.EnvironSizesGet,
+                proc_exit = ProcessExit.Throw
+            ))
+
+        instance._start()
+    with
+    | :? ProcessExitException as e when e.ExitCode = 0 -> ()
