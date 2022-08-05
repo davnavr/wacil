0c0d7d901a6f5ba013a4fe4a5261bae324a5828f
David N <davidcalieln@gmail.com>
2022-08-04 23:00:48 -0700

    Update logger to print message in finally

--- src/Wacil.Runtime.Wasi/Snapshot/Preview1/Logger.cs
+++ src/Wacil.Runtime.Wasi/Snapshot/Preview1/Logger.cs
@@ -19,36 +19,56 @@ public sealed class Logger {
     /// </summary>
     public Logger() : this(System.Console.Out) { }
 
+    private void WriteResult(int? result) {
+        output.Write(" -> ");
+        output.WriteLine(result == null ? "!" : (Errno)result);
+    }
+
     /// <summary>Wraps an implementation of the <c>args_get</c> function.</summary>
     public Func<int, int, int> ArgsGet(Func<int, int, int> getter) => (int argv, int buffer) => {
-        output.Write("args_get(argv = (byte**)0x{0:X}, argv_buf = (byte*)0x{1:X})", argv, buffer);
-        int result = getter(argv, buffer);
-        output.WriteLine(" -> {0}", (Errno)result);
-        return result;
+        int? result = null;
+        try {
+            result = getter(argv, buffer);
+            return result.Value;
+        } finally {
+            output.Write("args_get(argv = (byte**)0x{0:X}, argv_buf = (byte*)0x{1:X})", argv, buffer);
+            WriteResult(result);
+        }
     };
 
     /// <summary>Wraps an implementation of the <c>args_sizes_get</c> function.</summary>
     public Func<int, int, int> ArgsSizesGet(Func<int, int, int> getter) => (int count, int size) => {
-        output.Write("args_sizes_get(count = (byte*)0x{0:X}, size = (byte*)0x{1:X})", count, size);
-        int result = getter(count, size);
-        output.WriteLine(" -> {0}", (Errno)result);
-        return result;
+        int? result = null;
+        try {
+            result = getter(count, size);
+            return result.Value;
+        } finally {
+            output.Write("args_sizes_get(count = (byte*)0x{0:X}, size = (byte*)0x{1:X})", count, size);
+            WriteResult(result);
+        }
     };
 
     /// <summary>Wraps an implementation of the <c>environ_get</c> function.</summary>
     public Func<int, int, int> EnvironGet(Func<int, int, int> getter) => (int environ, int buffer) => {
-        output.Write("environ_get(environ = (byte**)0x{0:X}), argv_buf = (byte*)0x{1:X})", environ, buffer);
-        int result = getter(environ, buffer);
-        output.WriteLine(" -> {0}", (Errno)result);
-        return result;
+        int? result = null;
+        try {
+            result = getter(environ, buffer);
+            return result.Value;
+        } finally {
+            output.Write("environ_get(environ = (byte**)0x{0:X}), argv_buf = (byte*)0x{1:X})", environ, buffer);
+        }
     };
 
     /// <summary>Wraps an implementation of the <c>environ_sizes_get</c> function.</summary>
     public Func<int, int, int> EnvironSizesGet(Func<int, int, int> getter) => (int count, int size) => {
-        output.Write("environ_sizes_get(count = (byte*)0x{0:X}, size = (byte*)0x{1:X})", count, size);
-        int result = getter(count, size);
-        output.WriteLine(" -> {0}", (Errno)result);
-        return result;
+        int? result = null;
+        try {
+            result = getter(count, size);
+            return result.Value;
+        } finally {
+            output.Write("environ_sizes_get(count = (byte*)0x{0:X}, size = (byte*)0x{1:X})", count, size);
+            WriteResult(result);
+        }
     };
 
     /// <summary>Wraps an implementation of the <c>proc_exit</c> function.</summary>
@@ -60,19 +80,26 @@ public sealed class Logger {
     /// <summary>Wraps an implementation of the <c>fd_write</c> function.</summary>
     public Func<int, int, int, int, int> FdWrite(Func<int, int, int, int, int> writer) {
         return (int fd, int ioVectorPointer, int ioVectorCount, int bytesWrittenPointer) => {
-            output.Write("fd_write(fd = {0}, iovs = (ciovec*)0x{1:X}), iovs_len = {2}, ", fd, ioVectorPointer, ioVectorCount);
-            output.Write("nwritten = (int*)0x{0:X})", bytesWrittenPointer);
-            int result = writer(fd, ioVectorPointer, ioVectorCount, bytesWrittenPointer);
-            output.WriteLine(" -> {0}", (Errno)result);
-            return result;
+            int? result = null;
+            try {
+                result = writer(fd, ioVectorPointer, ioVectorCount, bytesWrittenPointer);
+                return result.Value;
+            } finally {
+                output.Write("fd_write(fd = {0}, iovs = (ciovec*)0x{1:X}), iovs_len = {2}, nwritten = (int*)0x{3:X})", fd, ioVectorPointer, ioVectorCount, bytesWrittenPointer);
+                WriteResult(result);
+            }
         };
     }
 
     /// <summary>Wraps an implementation of the <c>fd_close</c> function.</summary>
     public Func<int, int> FdClose(Func<int, int> implementation) => (int fd) => {
-        output.Write("fd_close(fd = {0})", fd);
-        int result = implementation(fd);
-        output.WriteLine(" -> {0}", (Errno)result);
-        return result;
+        int? result = null;
+        try {
+            result = implementation(fd);
+            return result.Value;
+        } finally {
+            output.Write("fd_close(fd = {0})", fd);
+            WriteResult(result);
+        }
     };
 }
