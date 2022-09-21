4fb1837558c2be76403b02bf120bb83474300ec7
David N <davidcalieln@gmail.com>
2022-09-20 21:46:13 -0700

    Add more wacil-bindgen crates

--- wacil-bindgen/src/lib.rs
+++ wacil-bindgen/src/lib.rs
@@ -1,1 +1,4 @@
 #![doc = include_str!("../README.md")]
+#![no_std]
+
+pub use wacil_macro::wacil_bindgen;
---
+++ wacil-bindgen/wacil-backend/src/lib.rs
@@ -0,0 +1,1 @@
+//! Used to generate C# wrapper code for a Rust WebAssembly module translated to a .NET assembly by `wacil`.
