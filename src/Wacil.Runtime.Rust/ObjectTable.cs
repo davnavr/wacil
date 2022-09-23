0139f50762a9bc40d0d7c8f9d4ab2f71ac1da283
David N <davidcalieln@gmail.com>
2022-09-22 22:25:07 -0700

    Add rust runtime helper GetHashCode

--- src/Wacil.Runtime.Rust/ObjectTable.cs
+++ src/Wacil.Runtime.Rust/ObjectTable.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 
 /// <summary>Maps integer indices to object references.</summary>
 internal sealed class ObjectTable {
+    internal const int NullIndex = -1;
+
     private readonly object locker = new();
 
     private readonly List<object?> lookup = new(capacity: 8);
@@ -16,7 +18,7 @@ internal sealed class ObjectTable {
     internal int Insert(object o) {
         lock (locker) {
             if (o == null) {
-                return -1;
+                return NullIndex;
             } else if (next < 0) {
                 throw new InvalidOperationException("no indices remaining");
             }
@@ -35,17 +37,29 @@ internal sealed class ObjectTable {
     }
 
     internal object? Get(int index) {
+        if (index == NullIndex) {
+            return null;
+        }
+
         lock (locker) {
             return lookup[index];
         }
     }
 
-    internal void Remove(int index) {
-        lock (locker) {
-            if (index >= lookup.Count) {
-                throw new IndexOutOfRangeException("index too large");
-            }
+    /// <summary>
+    /// Calls <see cref="object.GetHashCode"/> for the <see cref="object"/> corresponding to the given <paramref name="index"/>.
+    /// </summary>
+    /// <exception cref="ArgumentException">Thrown if the <paramref name="index"/> is the <see cref="NullIndex"/>.</exception>
+    internal int GetHashCode(int index) {
+        return (Get(index) ?? throw new ArgumentException(nameof(index))).GetHashCode();
+    }
 
+    internal void Remove(int index) {
+        if (index == NullIndex) {
+            throw new ArgumentException("attempt to free object corresponding to null index");
+        }
+
+        lock (locker) {
             if (lookup[index] == null) {
                 throw new InvalidOperationException("double free detected for object #" + index);
             }
--- src/Wacil.Runtime.Rust/RuntimeHelpers.cs
+++ src/Wacil.Runtime.Rust/RuntimeHelpers.cs
@@ -1,9 +1,22 @@
 ﻿namespace Wacil.Runtime.Rust;
 
+using System;
+using System.Collections.Generic;
+
 /// <summary>Provides runtime support for Rust programs compiled with <c>wacil-bindgen</c>.</summary>
 public sealed class RuntimeHelpers {
     private readonly ObjectTable objects = new();
 
+    private readonly IReadOnlyDictionary<string, object> runtimeHelperImports;
+
     /// <summary>Initializes a new <see cref="RuntimeHelpers"/> instance.</summary>
-    public RuntimeHelpers() {}
+    public RuntimeHelpers() {
+        // Runtime functions, keep in sync with wacil-bindgen/src/runtime.rs
+        runtimeHelperImports = new Dictionary<string, object>() {
+            { "wacil_rt_object_table_drop", new Action<int>(objects.Remove) },
+            { "wacil_rt_object_get_hash_code", new Func<int, int>(objects.GetHashCode) },
+        };
+    }
+
+
 }
