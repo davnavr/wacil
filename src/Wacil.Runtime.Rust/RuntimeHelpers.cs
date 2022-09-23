a9b658c70b8e0fa0a43c836ed6f4b29a210a0fea
David N <davidcalieln@gmail.com>
2022-09-23 15:43:15 -0700

    Add IDynamicImportResolver.cs

--- src/Wacil.Runtime.Rust/RuntimeHelpers.cs
+++ src/Wacil.Runtime.Rust/RuntimeHelpers.cs
@@ -1,22 +1,39 @@
 ﻿namespace Wacil.Runtime.Rust;
 
 using System;
-using System.Collections.Generic;
 
 /// <summary>Provides runtime support for Rust programs compiled with <c>wacil-bindgen</c>.</summary>
 public sealed class RuntimeHelpers {
     private readonly ObjectTable objects = new();
 
-    private readonly IReadOnlyDictionary<string, object> runtimeHelperImports;
+    private sealed class ImportResolver : IDynamicImportResolver {
+        private readonly IDynamicImportResolver imports;
 
-    /// <summary>Initializes a new <see cref="RuntimeHelpers"/> instance.</summary>
-    public RuntimeHelpers() {
-        // Runtime functions, keep in sync with wacil-bindgen/src/runtime.rs
-        runtimeHelperImports = new Dictionary<string, object>() {
-            { "wacil_rt_object_table_drop", new Action<int>(objects.Remove) },
-            { "wacil_rt_object_get_hash_code", new Func<int, int>(objects.GetHashCode) },
-        };
+        private readonly Action<int> rtObjectTableDrop;
+
+        private readonly Func<int, int> rtObjectGetHashCode;
+
+        internal ImportResolver(ObjectTable objects, IDynamicImportResolver imports) {
+            this.imports = imports;
+            this.rtObjectTableDrop = objects.Remove;
+            this.rtObjectGetHashCode = objects.GetHashCode;
+        }
+
+        public object? Resolve(string name) {
+            // Runtime functions, keep in sync with wacil-bindgen/src/runtime.rs
+            return name switch {
+                "wacil_rt_object_table_drop" => rtObjectTableDrop,
+                "wacil_rt_object_get_hash_code" => rtObjectGetHashCode,
+                _ => imports.Resolve(name),
+            };
+        }
     }
 
+    /// <summary>Initializes a new <see cref="RuntimeHelpers"/> instance.</summary>
+    public RuntimeHelpers() {}
 
+    /// <summary>Initializes an instance of the <typeparamref name="T"/> class.</summary>
+    public T InitializeImports<T>(IDynamicImportResolver imports) where T : class {
+        return Wacil.Runtime.DynamicImport.Create<T>(new ImportResolver(objects, imports));
+    }
 }
