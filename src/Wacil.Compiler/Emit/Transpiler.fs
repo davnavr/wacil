588ddc35ae3fba7d3360cb7c0c65c06c4098d40a
David N <davidcalieln@gmail.com>
2022-12-01 21:46:49 -0800

    Add translation for I64Load16U

--- src/Wacil.Compiler/Emit/Transpiler.fs
+++ src/Wacil.Compiler/Emit/Transpiler.fs
@@ -358,6 +358,11 @@ let translateWebAssembly
                 il.Add(CilInstruction.CreateLdcI4(int32 arg.Offset))
                 il.Add(CilInstruction(CilOpCodes.Call, instantiation.ReadByte))
                 il.Add(CilInstruction CilOpCodes.Conv_U8)
+            | I64Load16U arg ->
+                let instantiation = emitPushMemory arg.Memory il
+                il.Add(CilInstruction.CreateLdcI4(int32 arg.Offset))
+                il.Add(CilInstruction(CilOpCodes.Call, instantiation.ReadInt16))
+                il.Add(CilInstruction CilOpCodes.Conv_U4)
             | I64Load32U arg ->
                 let instantiation = emitPushMemArg arg il
                 il.Add(CilInstruction(CilOpCodes.Call, instantiation.ReadInt32))
